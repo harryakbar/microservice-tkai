f212e27f6faa563bdec0faa2767f93bba16f3d9e
rimanirfan <irfanriman@gmail.com>
2018-12-22 10:36:20 +0700

    set up urls

--- backend/addressbook/views.py
+++ backend/addressbook/views.py
@@ -3,6 +3,6 @@ from rest_framework import viewsets
 from .serializers import AddressBookSerializer
 from .models import AddressBook               
 
-class AddressBookSerializer(viewsets.ModelViewSet): 
+class AddressBookView(viewsets.ModelViewSet): 
     serializer_class = AddressBookSerializer    
     queryset = AddressBook.objects.all()  
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include            
+from rest_framework import routers               
+from addressbook import views                       
+
+router = routers.DefaultRouter()                 
+router.register(r'addresses', views.AddressBookView, 'addressbook')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),         
+    path('api/', include(router.urls))           
 ]
