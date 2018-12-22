a7c8fbe92efc7d50c3b19982d0b64b788a72f2ec
rimanirfan <irfanriman@gmail.com>
2018-12-22 10:27:33 +0700

    init djangorestframework and corsheaders

--- backend/addressbook/admin.py
+++ backend/addressbook/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import AddressBook
 
 # Register your models here.
+
+class AddressBookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'address', 'zip_code')
+
+admin.site.register(AddressBook, AddressBookAdmin)
