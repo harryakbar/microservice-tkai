0ba9662daa954152df42a710e7893b7e8bada0e4
rimanirfan <irfanriman@gmail.com>
2018-12-22 10:41:29 +0700

    change phone model to charfield

--- backend/addressbook/models.py
+++ backend/addressbook/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from phone_field import PhoneField
 
 # Create your models here.
 
 class AddressBook(models.Model):
     name = models.CharField(max_length=120)
-    phone = PhoneField(blank=True,help_text="Contact phone number")
+    phone = models.CharField(max_length=12)
     address = models.TextField()
     zip_code = models.CharField(max_length=5)
 
